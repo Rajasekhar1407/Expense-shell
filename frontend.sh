babf9eb6621bd5f0b64e3833f85ee9c3b9767cc1
Rajasekhar Reddy <brreddy1407@gmail.com>
2024-05-01 11:59:27 +0530

    Expense Project

--- frontend.sh
+++ frontend.sh
@@ -1,19 +1,53 @@
-echo "Test"
+#!/bin/bash
 
-dnf install nginx -y
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
-systemctl enable nginx
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
-systemctl start nginx
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R Please run the script with root access $N"
+    exit 1
+else
+    echo -e "$G You are Super User $N"
+fi
 
-rm -rf /usr/share/nginx/html/*
+VALIDATE() {
+    if [ $? -ne 0 ]
+    then
+        echo -e "$R $2...FAILED $N"
+    else
+        echo -e "$G $2...SUCCESS $N"
+    fi
+}
 
-curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip
+dnf install nginx -y &>>$LOGFILE
+VALIDATE $? "Installing Nginx"
+
+systemctl enable nginx &>>$LOGFILE
+VALIDATE $? "Enabling Nginx"
+
+systemctl start nginx &>>$LOGFILE
+VALIDATE $? "Starting Nginx"
+
+rm -rf /usr/share/nginx/html/* &>>$LOGFILE
+VALIDATE $? "Removing default content"
+
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOGFILE
+VALIDATE $? "Downloading frontend code"
 
 cd /usr/share/ngix/html
+unzip /tmp/frontend.zip &>>$LOGFILE
+VALIDATE $? "Extracting Frontend code"
 
-unzip /tmp/frontend.zip
+cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
+VALIDATE $? "Copieng expense conf"
 
-cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf
-
-systemctl restart nginx
+systemctl restart nginx &>>$LOGFILE
+VALIDATE $? "Restatting Nginx"
